49a9de021f220e52f23f8fa3fdcbf32a58a5c241
Coby Walsh <cobywalsh7@yahoo.com>
2025-05-01 08:12:41 -0600

    changed the shape from a cube to a torus

--- main.js
+++ main.js
@@ -11,30 +11,23 @@ document.body.appendChild( renderer.domElement );
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('assets/images/fur.jpg');
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = [
-    new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ map: texture}),
-    new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ map: texture })
-];
-const cube = new THREE.Mesh( geometry, material );
+const geometry = new THREE.TorusGeometry(1, 0.4, 16, 100);
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const torus = new THREE.Mesh( geometry, material );
+
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5); // position of light
 scene.add(directionalLight);
 
 
-scene.add( cube );
+scene.add( torus);
 
 camera.position.z = 5;
 
 function animate() {
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
 
   renderer.render( scene, camera );
 
